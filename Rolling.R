bff7303505420293c0fb634e3971dc965f95457f
mokainzi <aalmokainzi@gmail.com>
2020-09-20 17:09:02 +0300

    Added description

--- Rolling.R
+++ Rolling.R
@@ -1,3 +1,12 @@
+# Homework
+
+# Using MatrixDS or R-Studio Cloud
+# 1. Create a 10 sided dice and a 20 sided dice. 
+# 2. Create a function to roll 6 of these dice at a time and calculate the sum and
+# 3. another function to calculate how many dice rolled more than 6 (for the 10 sided) or 16 (for the 20 sided).
+# 4. Make sure to save all of your functions on a script called rolling.R 
+# 5. Upload your rolling.R script
+
 die10 <- 1:10
 die20 <- 1:20
 
